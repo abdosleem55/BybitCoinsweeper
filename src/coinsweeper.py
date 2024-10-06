73ff4066d1956b7408ce4c18d21c84f7cec95393
abdo sleem <xd.sleem@gmail.com>
2024-10-06 05:47:11 +0300

    first commit

---
+++ src/coinsweeper.py
@@ -0,0 +1,267 @@
+from src.__init__ import *
+# from __init__ import *
+import requests
+
+import json
+
+class Coinsweeper:
+    def __init__(self, token, proxy, timeout):
+        self.token = token
+        # if proxy:
+        #     self.session.proxies.update(proxy)
+
+        self.proxy = proxy
+        self.timeout = timeout
+
+
+        self.accessToken = None
+        self.refreshToken = None
+        self.game_id = None
+
+        self.user_id = None
+
+        self.hash = None
+        self.score = None
+        self.gameTime = None
+
+    def login(self):
+        json_data = {
+            'initData': self.token,
+        }
+
+        response = requests.post('https://api.bybitcoinsweeper.com/api/auth/login', json=json_data, proxies=self.proxy)
+
+        if response.status_code != 201:
+            return 'fail'
+
+        data = response.json()
+
+        self.accessToken = data['accessToken']
+
+        self.refreshToken = data['refreshToken']
+
+
+        return 'success'
+
+
+    def refresh_token(self):
+        
+        headers = {
+            'authority': 'api.bybitcoinsweeper.com',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'en-EG,en;q=0.9,ar-EG;q=0.8,ar;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+            'authorization': f'Bearer {self.accessToken}',
+            'content-type': 'application/json',
+            'origin': 'https://bybitcoinsweeper.com',
+            'referer': 'https://bybitcoinsweeper.com/',
+            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'tl-init-data': self.token,
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        }
+
+        json_data = {
+            'refreshToken': self.refreshToken
+        }
+
+        response = requests.post('https://api.bybitcoinsweeper.com/api/auth/refresh-token', headers=headers, json=json_data, proxies=self.proxy)
+
+        data = response.json()
+
+        self.accessToken = data['accessToken']
+        self.refreshToken = data['refreshToken']
+
+    def user_info(self):
+        
+        headers = {
+            'authority': 'api.bybitcoinsweeper.com',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'en-EG,en;q=0.9,ar-EG;q=0.8,ar;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+            'authorization': f'Bearer {self.accessToken}',
+            'origin': 'https://bybitcoinsweeper.com',
+            'referer': 'https://bybitcoinsweeper.com/',
+            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'tl-init-data': self.token,
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        }
+
+        response = requests.get('https://api.bybitcoinsweeper.com/api/users/me', headers=headers, proxies=self.proxy)
+        # response = self.session.get('https://api.bybitcoinsweeper.com/api/users/me')
+        
+        if response.status_code == 401:
+            self.refresh_token()
+            return self.user_info()
+        
+        if response.status_code != 200:
+            return 'fail'
+        
+        res = response.json()
+        self.user_id = res['id']
+        res['success'] = True
+        return res
+    
+    def start_game(self):
+        
+        headers = {
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'en,en-EG;q=0.9,ar-EG;q=0.8,ar;q=0.7,en-US;q=0.6',
+            'authorization': f'Bearer {self.accessToken}',
+            'cache-control': 'no-cache',
+            'origin': 'https://bybitcoinsweeper.com',
+            'pragma': 'no-cache',
+            'priority': 'u=1, i',
+            'referer': 'https://bybitcoinsweeper.com/',
+            'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Android WebView";v="128"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'tl-init-data': self.token,
+            'user-agent': 'Mozilla/5.0 (Linux; Android 13; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.148 Mobile Safari/537.36',
+            'x-requested-with': 'org.telegram.messenger',
+        }
+        # self.session.headers.update(headers)
+        response = requests.post('https://api.bybitcoinsweeper.com/api/games/start', headers=headers, proxies=self.proxy)
+        
+        if response.status_code == 401:
+            self.refresh_token()
+            return self.start_game()
+
+
+        self.game_response = response.json()
+        if response.status_code == 201:
+            return 'success'
+        
+        return 'fail'
+    
+    def win_game(self):
+        
+        headers = {
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'en,en-EG;q=0.9,ar-EG;q=0.8,ar;q=0.7,en-US;q=0.6',
+            'authorization': f'Bearer {self.accessToken}',
+            'cache-control': 'no-cache',
+            'content-type': 'application/json',
+            'origin': 'https://bybitcoinsweeper.com',
+            'pragma': 'no-cache',
+            'priority': 'u=1, i',
+            'referer': 'https://bybitcoinsweeper.com/',
+            'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Android WebView";v="128"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'tl-init-data': self.token,
+            'user-agent': 'Mozilla/5.0 (Linux; Android 13; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.148 Mobile Safari/537.36',
+            'x-requested-with': 'org.telegram.messenger',
+        }
+
+        json_data = {
+            'gameId': self.game_response['id'],
+            'bits': self.game_response['rewards']['bits'],
+            'bagCoins': self.game_response['rewards']['bagCoins'],
+            'gifts': self.game_response['rewards']['gifts'],
+            'score': self.game['score'],
+            'gameTime': self.gameTime,
+            'h': self.game['hash']
+        }
+        
+        response = requests.post('https://api.bybitcoinsweeper.com/api/games/win', headers=headers, json=json_data, proxies=self.proxy)
+        
+        if response.status_code == 401:
+            self.refresh_token()
+            return self.win_game()
+        
+
+        if response.status_code == 201:
+            return 'success'
+        return 'fail'
+
+    def lose_game(self):
+        headers = {
+            'authority': 'api.bybitcoinsweeper.com',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'en,en-EG;q=0.9,ar-EG;q=0.8,ar;q=0.7,en-US;q=0.6',
+            'authorization': f'Bearer {self.accessToken}',
+            'cache-control': 'no-cache',
+            'content-type': 'application/json',
+            'origin': 'https://bybitcoinsweeper.com',
+            'pragma': 'no-cache',
+            'priority': 'u=1, i',
+            'referer': 'https://bybitcoinsweeper.com/',
+            'sec-ch-ua': '"Android WebView";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'tl-init-data': self.token,
+            'user-agent': 'Mozilla/5.0 (Linux; Android 13; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.81 Mobile Safari/537.36',
+            'x-requested-with': 'org.telegram.messenger',
+        }
+
+        json_data = {
+            'gameId': self.game_id,
+            'bits': self.game_response['rewards']['bits'],
+            'bagCoins': self.game_response['rewards']['bagCoins'],
+            'gifts': self.game_response['rewards']['gifts'],
+        }
+
+        response = requests.post('https://api.bybitcoinsweeper.com/api/games/lose', headers=headers, json=json_data, proxies=self.proxy)
+        if response.status_code == 401:
+            self.refresh_token()
+            return self.lose_game()
+        
+        if response.status_code == 201:
+            return 'success'
+        return 'fail'
+    
+    def game_data(self, key, gameTime):
+        self.gameTime = gameTime
+        url = config('API', 'NOT SET')+'/Coinsweeper/api/v1/play'
+        with open('test.json', 'w') as f:
+            json.dump(self.game_response, f)
+        
+        data = {
+            'key': key,
+            'game_response': self.game_response,
+            'user_id': self.user_id,
+            'game_time': gameTime
+        }
+        
+        if url == 'NOT SET':
+            log(f'{Colors.RED} Please Set the API !')
+            return 'fail', 1
+
+        response = requests.get(url, json=data, timeout=self.timeout, proxies=self.proxy)
+        res = response.json()
+        if res['message']=='success':
+            self.game = res['game']
+            return 'success', response.status_code
+        
+        return 'fail', response.status_code
+    
+
+if __name__ == '__main__':
+    tokens = load_tokens()
+    x = Coinsweeper(tokens[0], random_proxy(), config('TIMEOUT', 6))
+    x.login()
+    
+    print(x.user_info())
+    # x.refresh_token()
+    # x.start_game()
+
+
+
+
---
+++ src/core.py
@@ -0,0 +1,214 @@
+from src.__init__ import *
+from src.coinsweeper import Coinsweeper
+def menu():
+    print(f"         {Colors.MAGENTA}=> Main Menu\n")
+    menu_item(1, "Start Script")
+    menu_item(2, 'Edit Config')
+    menu_item(3, 'About Devolopers')
+    menu_item(99, 'Exit')
+    options = {
+        '1': start_script,
+        '2': edit_config,
+        '3': about_devoloper,
+        '99': exit_code
+    }
+    print()
+    while True:
+        choice = input(f'{Colors.CYAN} Choice : ')
+        if choice not in options:
+            print(f'{Colors.YELLOW} Choose Valid Option')
+            continue
+        break
+
+    options[choice]()
+        
+def start_script():
+    while True:
+        awak()
+        tokens = load_tokens()
+        log(Colors.CYAN + f' Number of Accounts {Colors.GREEN}{len(tokens)}')
+        
+        for index, token in enumerate(tokens):
+            try:
+                log_line()
+                countdown_timer(random.randint(config('MIN_TIME_BETWEEN_ACCOUNTS', 10), config('MAX_TIME_BETWEEN_ACCOUNTS', 15)))
+                account = Coinsweeper(token, random_proxy(), config('TIMEOUT', 6))
+                log(f'{Colors.BLUE} Account Number : {Colors.GREEN}{index+1}')
+                log(f'{Colors.BLUE} Account Username : {Colors.GREEN}{get_username(token)}')
+                log(f'{Colors.YELLOW} Trying To Login ...')
+                res = account.login()
+
+                if res == 'success': log(f'{Colors.GREEN} Done Login Success !')
+                else:
+                    if res == 'fail':
+                        log(f'{Colors.RED} Faild To Login Please Check Account Token !')
+                    else:
+                        log(f'{Colors.BLACK} {res}')
+                        log(f'{Colors.RED} UnExpected Erro Please Contact With Devoloper To Fix !')
+                    continue
+                
+                # res = account.daily_login()
+
+                # if res == 'claimed':
+                #     log(f'{Colors.BLUE} Daily login {Colors.GREEN}Claimed !')
+                # elif res=='already_claimed':
+                #     log(f'{Colors.BLUE} Daily login {Colors.GREEN}Aleady Claimed !')
+                # else:
+                #     log(f'{Colors.RED} {res}')
+
+                for __ in range(config(random.randint(config('MIN_GAMES_PER_ACCOUNT', 4), config('MAX_GAMES_PER_ACCOUNT', 9)))):
+                    res = account.user_info()
+
+                    if not res['success']:
+                        log(f'{Colors.RED} Faild To Retrive Account info !')
+                        break
+                    time.sleep(3)
+                    score = res['score']
+                    log(f'{Colors.BLACK} ---------------------------------')
+                    log(f"{Colors.BLUE} Account Balance : {Colors.GREEN}{score}")
+
+                    countdown_timer(config('SMALL_DELAY', 3))
+
+                    log(f' {Colors.BLUE}Starting game !')
+                    res = account.start_game()
+                    
+                    if res == 'success': log(f' {Colors.GREEN}Done Game Has Started !')
+                    else:
+                        log(f' {Colors.BLACK} {res}')
+                        log(f' {Colors.RED} UnExpected Erro Please Contact With Devoloper To Fix !')
+                        continue
+                    
+                    gameTime = random.randint(config('MIN_GAME_TIME', 65), config('MAX_GAME_TIME', 185))
+                    # gameTime = 60
+                    # countdown_timer(gameTime)
+                    countdown_timer(config('SMALL_DELAY', 3))
+
+                    key = config('KEY', 'NOT SET')
+                    if key == 'NOT SET':
+                        log(f'{Colors.RED} Please Set Access Key First.')
+                        exit_code()
+                    
+                    log(f' {Colors.BLUE}Getting game data ... ')
+                    
+                    retry = config('MAX_RETRY', 3)
+                    for __ in range(retry):
+                        res, status_code = account.game_data(key, gameTime)
+                        if res == 'success':
+                            log(f' {Colors.GREEN}Done Game Data Dumped !')
+                            retry = 0
+                            break
+                        
+                        if status_code==429:
+                            log(f'{Colors.RED} Rate Limit For this key !')
+                            retry = 1
+                            break
+                        
+                        if status_code==403:
+                            log(f'{Colors.RED} This key is Not Active !')
+                            retry = 1
+                            break
+
+                        if status_code==401:
+                            log(f'{Colors.RED} Invaild Key !')
+                            retry = 1
+                            break
+                        
+                        if status_code==400:
+                            log(f'{Colors.RED} Key is Required !')
+                            retry = 1
+                            break
+                    
+                    if retry:
+                        log(f' {Colors.RED}Faild To Dump Game Data !')
+                        log(f' {Colors.RED}{res}')
+                        continue
+
+                    # countdown_timer(config('SMALL_DELAY', 3))
+                    
+                    countdown_timer(gameTime-lost_time(account.game_response['createdAt']))
+
+                    is_win = choose_based_on_percentage(config('WINNING_PERCENTAGE', 80))
+                    
+                    if is_win:
+                        log(f' {Colors.BLUE}Wining The Game ...')
+                        res = account.win_game()
+                    else:
+                        log(f' {Colors.BLUE}Losing The Game ...')
+                        res = account.lose_game()
+
+                    if res == 'success':
+                        log(f' {Colors.GREEN}Success + {int(account.game["score"]*is_win)} (^__*)')
+                    else:
+                        log(f' {Colors.RED}Faild To Finish The Game !')
+                        log(f' {Colors.RED}If This message appear more than onece Please Tell The Devoloer [WARRING] !')
+                        continue
+                    
+                    countdown_timer(random.randint(config('MIN_DELAY_AFTER_GAME', 5), config('MAX_DELAY_AFTER_GAME', 9)))
+
+                countdown_timer(config('SMALL_DELAY', 3))
+            except KeyboardInterrupt:
+                exit_code()
+            except Exception as E:
+                log(f'{Colors.RED} {E}')
+        countdown_timer(random.randint(config('MIN_DELAY_BEFORE_RESTART', 700), config('MAX_DELAY_BEFORE_RESTART', 1000)))
+            
+
+def edit_config():
+    awak()
+    config_values = read_config()
+    i=1
+    print(f"         {Colors.MAGENTA}=> Edit Config\n")
+    conf = []
+    for key, val in config_values.items():
+        menu_item(i, key+Colors.YELLOW+' : '+Colors.GREEN+str(val))
+        conf.append([key, val])
+        i += 1
+    menu_item(99, f'{Colors.YELLOW}Back\n')
+
+    while True:
+        choice = input(f'{Colors.CYAN} Choice : ')
+        if choice == '99':
+            awak()
+            menu()
+            return
+        try:int(choice)
+        except:
+            print(f'{Colors.YELLOW} Choose Valid Intger')
+            continue
+
+        if int(choice)<1 or int(choice)>len(conf):
+            print(f'{Colors.YELLOW} Choose Valid Option')
+            continue
+        break
+
+    choice = int(choice)-1
+    while True:
+        value = input(f'{Colors.GREEN} {conf[choice][0]} = ')
+        
+        if not value.strip():
+            print(f'{Colors.YELLOW} Enter Vaild Value !')
+            continue
+        break
+    
+    try: value = eval(value)
+    except: pass
+    edit_config_value(conf[choice][0], value)
+
+    awak()
+    menu()
+
+    
+def about_devoloper():
+    awak()
+    print(f"{Colors.BLUE} Devoloper : {Colors.GREEN}Abdo Sleem")
+    print(f"{Colors.BLUE} Github : {Colors.GREEN}https://github.com/scriptvip")
+    print(f"{Colors.BLUE} Telegram : {Colors.GREEN}https://t.me/glitch_no")
+    log_line()
+    input(f"\n   {Colors.YELLOW} Press Enter To Back !")
+    awak()
+    menu()
+
+def exit_code():
+    log_line()
+    log(f'{Colors.RED} Exiting ...')
+    exit()
